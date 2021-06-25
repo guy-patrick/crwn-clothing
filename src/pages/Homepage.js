c74df2192db51fc703a7771458e25f7d983a03ef
Patrick Ouambo <patgp04@gmail.com>
2021-06-25 11:38:56 +0100

    first commit on this project

---
+++ src/pages/Homepage.js
@@ -0,0 +1,7 @@
+import Directory from "../components/Directory";
+
+export const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
---
+++ src/root/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import { Homepage } from "../pages/Homepage";
+import "./App.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Homepage />
+      </div>
+    );
+  }
+}
+
+export default App;
